3fea6b231a7f3443eec99f7d38d64c3efea56318
cesar <cesar.ferreira.tn@gmail.com>
2018-01-03 20:10:27 -0300

    commit initial

---
+++ app/src/main/java/com/example/cesar/portraitandland/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.cesar.portraitandland;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button)
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onClick() {
+
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    }
+}
